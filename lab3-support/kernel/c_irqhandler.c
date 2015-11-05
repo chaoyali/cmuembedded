e563a02ce25f69fcf74e5eed597cc9a635bc7928
RosyGAO <gaoyarosy@gmail.com>
2015-11-04 20:04:41 -0500

    Update c_irqhandler.c

--- lab3-support/kernel/c_irqhandler.c
+++ lab3-support/kernel/c_irqhandler.c
@@ -4,7 +4,7 @@
  * @handler the irq
  *
  * @Chaoya Li	<chaoyal@andrew.cmu.edu>
- * @Ya Gao 		<yag1@andrew.cmu.edu>
+ * @Ya Gao 	<yag1@andrew.cmu.edu>
 **/
 #include <exports.h>
 
@@ -18,6 +18,14 @@
 
 //dispatch the handler
 void C_IRQ_handler(){
-
+	/* Check that a time interrupt has occured, if not, ignore it */
+	/* Check if interrupt controller irq pending reg. bit 26 is set */
+	if (reg_read(INT_ICIP_ADDR) & (1 << 26)) {
+		/* Check if OSMR0 = OSCR */
+		if (reg_read(OSTMR_OSSR_ADDR) & 0x1) {
+			/* Reload the match register */
+			
+		}
+	}
 	printf("ok");
 }

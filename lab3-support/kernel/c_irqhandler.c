5e53a493878026baa7dcac118187728a9664552a
chaoyal <chaoyal6@gmail.com>
2015-11-09 00:29:49 -0500

    need last

--- lab3-support/kernel/c_irqhandler.c
+++ lab3-support/kernel/c_irqhandler.c
@@ -12,20 +12,29 @@
 #include <arm/exception.h>
 #include <arm/interrupt.h>
 #include <arm/timer.h>
-
+#include <arm/reg.h>
 #include "./include/bits/swi.h"
 #include "./include/bits/errno.h"
 
+extern unsigned long sys_time;
 //dispatch the handler
 void C_IRQ_handler(){
 	/* Check that a time interrupt has occured, if not, ignore it */
 	/* Check if interrupt controller irq pending reg. bit 26 is set */
-	if (reg_read(INT_ICIP_ADDR) & (1 << 26)) {
+    if((reg_read(OSTMR_OSSR_ADDR) & OSTMR_OSSR_M0) && (reg_read(INT_ICPR_ADDR) >> INT_OSTMR_0)) {
 		/* Check if OSMR0 = OSCR */
-		if (reg_read(OSTMR_OSSR_ADDR) & 0x1) {
-			/* Reload the match register */
-			
-		}
+//		if (reg_read(OSTMR_OSSR_ADDR) & 0x1) {
+			// Increment time by 10ms
+			sys_time += 10;
+      		// Set OSMR to the next 10ms value
+
+			reg_write(OSTMR_OSCR_ADDR, 0);
+//      	reg_write(OSTMR_OSMR_ADDR(0), reg_read(OSTMR_OSMR_ADDR(0)) + 32500);
+
+      		// Writing a 1 to the bit acknowledges the match and hence, clears that bit
+			reg_set(OSTMR_OSSR_ADDR, OSTMR_OSSR_M0);
+//		}
 	}
-	printf("ok");
+
+
 }

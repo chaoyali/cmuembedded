5516022eed7ae2606e3e6aa13d2d797249fbb981
RosyGAO <gaoyarosy@gmail.com>
2015-12-01 15:13:29 -0500

    Update C_SWI_hander.c

--- lab4/kernel/arm/C_SWI_hander.c
+++ lab4/kernel/arm/C_SWI_hander.c
@@ -1,5 +1,5 @@
 #include <types.h>
-
+#include <lock.h>
 
 #include <arm/reg.h>
 #include <bits/swi.h>
@@ -20,29 +20,28 @@ void C_SWI_handler(unsigned swi_num, unsigned *regs){
 		case WRITE_SWI:
 			write_syscall(regs[0], (void *)regs[1], regs[2]); 
 			break;
-        case TIME_SWI:
-            regs[0] = sys_time;
-            break;
-        case SLEEP_SWI:
-            msecond = regs[0];
-            sleep_syscall(msecond);
-            break;
-       case CREATE_SWI:
+    case TIME_SWI:
+        regs[0] = sys_time;
+        break;
+    case SLEEP_SWI:
+        msecond = regs[0];
+        sleep_syscall(msecond);
+        break;
+    case CREATE_SWI:
    			task_create((task_t *)regs[0], regs[1]);
-            break;
-       case MUTEX_CREATE:
-   			
-            break;
-       case MUTEX_LOCK:
-   			
-            break;
-       case MUTEX_UNLOCK:
-   			
-            break;
-       case EVENT_WAIT:
+        break;
+    case MUTEX_CREATE:
+        regs[0] = mutex_create();
+        break;
+    case MUTEX_LOCK:
+        regs[0] = mutex_lock(regs[0]);
+        break;
+    case MUTEX_UNLOCK:
+        regs[0] = mutex_unlock(regs[0]);
+        break;
+    case EVENT_WAIT:
    			regs[0] = event_wait(regs[0]);
-            break;
-
+        break;
 		default:;
 	}
 }
